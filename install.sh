5dd1f4a0acca6d40a0d886bc32e3ea4e8957a3e0
Nikolay Stoynov <arvigeus@gmail.com>
2024-09-15 17:40:43 +0300

    go

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+# https://raw.githubusercontent.com/arvigeus/arch/master/install.sh
+
 sudo usermod -aG wheel arvigeus
 echo "%wheel ALL=(ALL:ALL) ALL" | sudo tee /etc/sudoers.d/wheel
 echo "%wheel ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/99_wheel_nopasswd
@@ -10,7 +14,7 @@ sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist\n" | sudo tee -a /etc/pacman.conf
-sudo pacman -Sy
+sudo pacman -Suy
 
 sudo pacman -S --needed --noconfirm chaotic-aur/paru
 sudo pacman -S --needed --noconfirm chaotic-aur/cosmic-session-git
@@ -20,4 +24,4 @@ flatpak remote-add flathub-beta https://flathub.org/beta-repo/flathub-beta.flatp
 
 flatpak install -y flathub io.github.zen_browser.zen
 
-sudo systemctl enable --now cosmic-greeter.service
+sudo systemctl enable cosmic-greeter.service
